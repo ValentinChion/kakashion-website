0e2402bbd5ef3cd860c9aa8187a22c40abb42e4e
ValentinChion <valentin.chion@gmail.com>
2019-04-14 17:56:04 +0200

    Refactor imports for lisibility

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import './App.css';
 import AppDisplayer from './AppDisplayer';
-import CSSTranslate from '../CSSHandle/CSSTranslate';
-import {getActiveCSS} from '../../utils/CSS';
 import BulmaNavbarDisplayer from '../Navbar/BulmaNavbarDisplayer';
 import NesNavbarDisplayer from '../Navbar/NesNavbarDisplayer';
+
+import CSSTranslate from '../CSSHandle/CSSTranslate';
+import {getActiveCSS} from '../../utils/CSS';
+import './App.css';
 import './blocks.scss';
+
 
 class App extends Component {
   state = {
